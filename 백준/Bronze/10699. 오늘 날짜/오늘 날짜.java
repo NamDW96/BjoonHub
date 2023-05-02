fb94ffd56669b8e3ecc1848f7ec0f0974b8ff397
Dongw <114648142+NamDW96@users.noreply.github.com>
2023-05-02 23:46:25 +0900

    [Bronze V] Title: 오늘 날짜, Time: 276 ms, Memory: 18708 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		SimpleDateFormat dformat = new SimpleDateFormat("YYYY-MM-dd");		
+				
+		System.out.println(dformat.format(date));
+	}
+}
