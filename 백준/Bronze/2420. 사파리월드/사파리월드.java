bac5a63d608ea7ca1a48a943510aee5fbf60fa68
Dongw <114648142+NamDW96@users.noreply.github.com>
2023-05-07 00:06:14 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+
+		long num = N-M;
+		
+		if(num >=0) {
+			System.out.println(num);
+		} else {
+			num = -num;
+			System.out.println(num);
+		}
+	}
+}
