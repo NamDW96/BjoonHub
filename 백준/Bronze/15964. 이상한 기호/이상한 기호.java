661ea0e2d5f34dd215856b1653ffa89b55075238
NamDW96 <114648142+NamDW96@users.noreply.github.com>
2023-05-08 23:30:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,14 +1,17 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println((a-b)*(a+b));
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		
+		System.out.println((a+b)*(a-b));
 	}
 }
