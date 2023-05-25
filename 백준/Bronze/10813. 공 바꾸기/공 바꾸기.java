7a63a1d39b758762ed0992c7670f2ff088947b8e
NamDW96 <114648142+NamDW96@users.noreply.github.com>
2023-05-25 17:32:43 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n+1];
+		int num =0;
+		for(int i=1; i<arr.length; i++) {
+			arr[i]=i;
+		}
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			num = arr[a];
+			arr[a] 	= arr[b];
+			arr[b] = num;
+			
+		}
+		for(int i=1; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
