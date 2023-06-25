b8522029df98b221b98c343b336c64d933f13b57
NamDW96 <114648142+NamDW96@users.noreply.github.com>
2023-06-25 23:55:40 +0900

    [Bronze V] Title: Rook, Time: 116 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws NumberFormatException, IOException{
+    	
+    	System.out.println("  ___  ___  ___");
+    	System.out.println("  | |__| |__| |");
+    	System.out.println("  |           |");
+    	System.out.println("   \\_________/");
+    	System.out.println("    \\_______/");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |_____|");
+    	System.out.println("  __/       \\__");
+    	System.out.println(" /             \\");
+    	System.out.println("/_______________\\");
+    
+    
+    }
+}
