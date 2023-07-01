a1a8f06e5866f1df4f93fe9ee0ab6db236c30f0d
NamDW96 <114648142+NamDW96@users.noreply.github.com>
2023-07-01 20:26:55 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 140 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<5; i++) {
+			sb.append(Integer.parseInt(st.nextToken()) - n*x+" ");
+		}
+		
+		System.out.println(sb);
+		
+		
+	}
+}
